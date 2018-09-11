b2006c9f7f1108b1a0f12c9875bdf1f107564dfd
wojiaozhupengfei <18710805399@163.com>
2018-09-12 07:41:10 +0800

    新建文件

---
+++ TensorFlow_Learing/04/ExponentialMovingAverage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/12/0012 7:19
+# @Author  : zhupengfei
+# @Site    : 
+# @File    : ExponentialMovingAverage.py
+# @Software: PyCharm
+
