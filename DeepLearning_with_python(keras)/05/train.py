40ac8790721d664916f54a6c785c6564a9c24208
wojiaozhupengfei <810020522@qq.com>
2018-11-29 20:37:29 +0800

    05

---
+++ DeepLearning_with_python(keras)/05/train.py
@@ -0,0 +1,10 @@
+#!-*- coding:utf-8 -*-
+# author:zhupengfei
+# datetime:2018/11/29 20:27
+# software:PyCharm
+# discribe:
+from nets import *
+from data_process import *
+
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator, validation_steps=50)
+model.save('cats_and_dogs_small_1.h5')
